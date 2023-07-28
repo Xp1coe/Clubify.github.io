73f2aa6a4438b002ac7f512de548de30b7ac6651
Xp1coe <202101622@vupune.ac.in>
2023-07-28 12:10:43 +0530

    Clubify

--- index.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function () {
-    var carousel = document.querySelector('.carousel');
-    var myCarousel = new Carousel(carousel, {
-        autoplay: true,
-        interval: 3000,
-        transition: 'slide'
-    });
-});
