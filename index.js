2fff26938410bf43d099bca983f6e0d6c5831a12
Xp1coe <202101622@vupune.ac.in>
2023-07-28 12:10:00 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
-var carosuel = document.querySelector('.carosuel');
-
 document.addEventListener('DOMContentLoaded', function () {
-
-})
+    var carousel = document.querySelector('.carousel');
+    var myCarousel = new Carousel(carousel, {
+        autoplay: true,
+        interval: 3000,
+        transition: 'slide'
+    });
+});
